bde85e0be3129b49e7179e79ae31c56b09b60f02
Александр Горшенев <42249910+Spurium@users.noreply.github.com>
2020-11-12 02:20:38 +0300

    собрал слайдер

--- js/script.js
+++ js/script.js
@@ -13,3 +13,49 @@ function closeModal() {
 
 buttonClose.addEventListener('click', closeModal);
 buttonOpen.addEventListener('click', openModal);
+
+const slider = document.querySelector('.carousel');
+
+if (slider) {
+  const carouselList = slider.querySelector('.carousel-list');
+  const controlOne = slider.querySelector('.first');
+  const controlTwo = slider.querySelector('.second');
+  const controlThree = slider.querySelector('.third');
+
+  controlOne.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    controlOne.classList.add('active-control');
+    carouselList.classList.add('slide-1');
+
+    controlTwo.classList.remove('active-control');
+    carouselList.classList.remove('slide-2');
+    controlThree.classList.remove('active-control');
+    carouselList.classList.remove('slide-3');
+  });
+
+  controlTwo.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    controlTwo.classList.add('active-control');
+    carouselList.classList.add('slide-2');
+
+    controlOne.classList.remove('active-control');
+    carouselList.classList.remove('slide-1');
+    controlThree.classList.remove('active-control');
+    carouselList.classList.remove('slide-3');
+  });
+
+  controlThree.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    controlThree.classList.add('active-control');
+    carouselList.classList.add('slide-3');
+
+    controlOne.classList.remove('active-control');
+    carouselList.classList.remove('slide-1');
+    controlTwo.classList.remove('active-control');
+    carouselList.classList.remove('slide-2');
+  });
+
+}
